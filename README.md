068ac93cac479d6c8fcd101884ed4691c86e88d3
Rahul Powar <rahulp@gmail.com>
2016-05-28 12:47:19 +0100

    Updated scripts

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Installation
+    
+    # Install pip if required
+    easy_install pip
+
+    pip install -r requirements.txt
+
 # Usage
 
     # Strace the ls command and write the syscalls to ls.out
@@ -13,5 +20,42 @@
     
     # Run the scratch image
     docker run -ti traced-ls
+
+# Help
+
+    usage: tracer.py [-h] [--chdir CHDIR] [--build BUILD] [--cmd CMD]
+                    [--entrypoint ENTRYPOINT] [--script SCRIPT]
+                    [--ldd | --no-ldd] [--nss | --no-nss] [--ids | --no-ids]
+                    files [files ...]
+
+    Process strace output and generate Dockerfile.
+
+    positional arguments:
+    files                 strace files to parse
+
+    optional arguments:
+    -h, --help            show this help message and exit
+    --chdir CHDIR         Initial working directory for strace file
+    --build BUILD         Directory to assemble Dockerfile in
+    --cmd CMD, -c CMD     Docker CMD
+    --entrypoint ENTRYPOINT, -e ENTRYPOINT
+                            Docker ENTRYPOINT
+    --script SCRIPT, -s SCRIPT
+                            Create a script file to assemble directory insead of
+                            doing it
+    --ldd                 Include LDD configuration
+    --no-ldd
+    --nss                 Include resolover services
+    --no-nss
+    --ids                 Include machine-id from host
+    --no-ids
+
+# Issues
+
+- Ensure your system `strace` is up to date. 
+- You typically cannot trace from inside a Docker container. 
+- Ensure you use the -ttt flag to create parseable timestamps.
+
+# Acknowledgements
 
 Includes a srtace parser from https://github.com/dirtyharrycallahan/pystrace and ELF parser from https://github.com/larsks/dockerize
--- tracer.py
+++ tracer.py
@@ -12,11 +12,14 @@ import shlex
 import json
 import errno
 import subprocess
+import datetime
 
 from jinja2 import Environment, PackageLoader
 from depsolver import DepSolver
 
-IGNORE = [ '/proc', '/proc/*', '/dev', '/dev/*', '/etc/ld.so.cache', '/app' ]
+IGNORE = [ '/proc', '/proc/*', '/dev', '/dev/*', '/etc/ld.so.cache' ]
+
+MKDIRS = [ '/root', '/tmp' ]
 
 COPYALL = [ '/etc/fonts', 
             '/usr/share/X11/xkb', 
@@ -24,19 +27,20 @@ COPYALL = [ '/etc/fonts',
             '/usr/share/poppler/cMap', 
             '/usr/share/zoneinfo' ]
 
-MKDIRS = [ '/root', '/tmp', '/var/lib', '/var/cache', '/usr/local/share/fonts' ]
+INCLUDE_ID = [  '/etc/machine-id', 
+                '/var/lib/dbus/machine-id' ]
+            
+            
+INCLUDE_LDD = [ '/usr/bin/ldd', 
+                '/bin/sh',
+                '/sbin/ldconfig',
+                '/etc/ld.so.conf',
+                '/etc/ld.so.conf.d' ]
 
-INCLUDE = [ '/etc/machine-id', 
-            '/var/lib/dbus/machine-id', 
-            '/usr/bin/ldd', 
-            '/bin/sh',
-            '/sbin/ldconfig',
-            '/etc/ld.so.conf',
-            '/etc/ld.so.conf.d', 
+INCLUDE_NSS = [             
             '/usr/lib64/libnss_dns.so.2', 
             '/usr/lib64/libnss_files.so.2', 
             '/usr/lib64/libnss_compat.so.2' ]
-
 
 def make_path_or_exists(path):
     try:
@@ -91,27 +95,40 @@ def expand_file(path, sym_set, files_set, found_set, no_cpy_all):
         for src in deps.deps:
             expand_file(src, sym_set, files_set, found_set, no_cpy_all)
 
-def generate_dockerfile(target, docker):
+def add_dir(path, mkdirs_set):
+    dirs = [n for n in COPYALL if fnmatch.fnmatch(path, os.path.join(n, '*'))]
+    if len(dirs) != 0:
+        for roots in dirs:
+            mkdirs_set.add(roots)    
+
+def generate_dockerfile(docker):
     env = Environment(loader=PackageLoader('tracer', 'templates'))
     
     tmpl = env.get_template('Dockerfile.jinja2')
-    with open(os.path.join(target, 'Dockerfile'), 'w') as df:
+    with open('Dockerfile', 'w') as df:
         df.write(tmpl.render(docker=docker))
 
-def copy_file(src, dir, dst=None):
+
+def make_path(path, scr):
+    if scr is not None:
+        scr.write('mkdir -p "%s"\n' % path)
+    else:
+        make_path_or_exists(path)
+
+def copy_file(src, dir, scr, dst=None):
     if dst is None:
         dst = src
 
     target = os.path.join(dir, dst[1:])
     target_dir = os.path.dirname(target)
 
-    make_path_or_exists(target_dir)
+    make_path(target_dir, scr)
+    cmd = ['rsync', '-aXcl', src, target]
 
-    cmd = ['rsync', '-a']
-    # Symlinks
-    cmd.append('-l')
-    cmd += [src, target]
-    subprocess.check_call(cmd)
+    if scr is not None:
+        scr.write('%s %s "%s" "%s"\n' % tuple(cmd))
+    else:
+        subprocess.check_call(cmd)
 
     
 def main():
@@ -122,16 +139,37 @@ def main():
     
     parser.add_argument('--cmd', '-c', help='Docker CMD')
     parser.add_argument('--entrypoint', '-e', help='Docker ENTRYPOINT')    
+
+    parser.add_argument('--script', '-s', help='Create a script file to assemble directory insead of doing it')
     
     parser.add_argument('files', nargs='+', help='strace files to parse')
-      
+ 
+    inc_ldd_parser = parser.add_mutually_exclusive_group(required=False)
+    inc_ldd_parser.add_argument('--ldd', dest='ldd', action='store_true', help='Include LDD configuration')
+    inc_ldd_parser.add_argument('--no-ldd', dest='ldd', action='store_false')
+    parser.set_defaults(ldd=True) 
+ 
+    inc_nss_parser = parser.add_mutually_exclusive_group(required=False)
+    inc_nss_parser.add_argument('--nss', dest='nss', action='store_true', help='Include resolover services')
+    inc_nss_parser.add_argument('--no-nss', dest='nss', action='store_false')
+    parser.set_defaults(nss=True) 
+ 
+    inc_id_parser = parser.add_mutually_exclusive_group(required=False)
+    inc_id_parser.add_argument('--ids', dest='ids', action='store_true', help='Include machine-id from host')
+    inc_id_parser.add_argument('--no-ids', dest='ids', action='store_false')
+    parser.set_defaults(ids=True) 
+          
     args = parser.parse_args()
     
-    if os.path.isabs(args.chdir) == False:
-        raise Exception('--chdir must be absolute')
+    in_files = [open(file, "r") for file in args.files]
+        
+#    if os.path.isabs(args.chdir) == False:
+#        raise Exception('--chdir must be absolute')
     
     make_path_or_exists(args.build)
-    root = os.path.join(args.build, 'root')
+    os.chdir(args.build)
+    
+    root = './root'
     
     docker = {}
     if args.cmd:
@@ -139,55 +177,101 @@ def main():
     
     if args.entrypoint:
         docker['entrypoint'] = json.dumps(shlex.split(args.entrypoint))
-           
-    generate_dockerfile(args.build, docker)
+            
+    included = []
     
-    for dir in MKDIRS:
-        make_path_or_exists(os.path.join(root, dir[1:]))
+    if args.ldd is True:
+        included += INCLUDE_LDD
+        docker['ldd'] = True
+        
+    if args.ids is True:
+        included += INCLUDE_ID
+        
+    if args.nss is True:
+        included += INCLUDE_NSS
+            
+    generate_dockerfile(docker)
             
     error_set = sets.Set()
     files_set = sets.Set()
     sym_set = sets.Set()
     found_set = sets.Set()
-    
-    for forced in INCLUDE:
+    mkdirs_set = sets.Set(MKDIRS)
+
+                       
+    for forced in included:
         expand_file(forced, sym_set, files_set, found_set, False)
+    
+    i = 0
         
-    for input_file in args.files:
+    for f_in in in_files:
         cdir = args.chdir
-        f_in = open(input_file, "r")
         strace_stream = StraceInputStream(f_in)
         for entry in strace_stream:
+            i = i + 1
+            sys.stdout.write('\r%i lines parsed' % (i))
+            sys.stdout.flush()
+    
             if entry is None:
                 continue
+                
             if entry.syscall_name in [ "chdir" ]:
                 ndir = entry.syscall_arguments[0]
                 cdir = abs_path(ndir[1:-1], cdir)
-                print(cdir)
+                if int(entry.return_value) < 0:
+                    error_set.add(path)
+                else:
+                    add_dir(cdir, mkdirs_set)
                 continue
-                
+            
+            if entry.syscall_name in [ "statfs" ]:
+                path = entry.syscall_arguments[0]
+                path = abs_path(path[1:-1], cdir)                
+                if int(entry.return_value) < 0:
+                    error_set.add(path)
+                else:
+                    add_dir(path, mkdirs_set)
+                    
             if entry.syscall_name in [ "open", "stat", "execve" ]:
                 path = entry.syscall_arguments[0]
                 path = abs_path(path[1:-1], cdir)
-                 
                 if int(entry.return_value) < 0:
                     error_set.add(path)
                 else:
                     expand_file(path, sym_set, files_set, found_set, False)
 
+    sys.stdout.write('\n')       
                     
     for path in sorted(error_set):
         if os.path.basename(path) not in found_set:
-            sys.stderr.write("Warning, open error: %s\n" % path)    
-        
-    for path in sorted(files_set):   
-        copy_file(path, root)
+            sys.stderr.write("Warning, open error in trace: %s\n" % path)    
 
-    for path in sorted(sym_set):   
-        copy_file(path, root)
+    scr = None
+    if args.script is not None:
+        f = args.script
+        scr = open(f, 'w')
+        st = os.stat(f)
+        os.chmod(f, st.st_mode | 0111)
+        scr.write('#!/bin/sh\n\n# Auto generated by tracer.py on %s\n\n' % datetime.datetime.utcnow())
+
+    print('Making %i dirs' % len(mkdirs_set))
+    for dir in mkdirs_set:
+        make_path(os.path.join(root, dir[1:]), scr)
     
-    print('Files copied: %i, Links copied: %i' % (len(files_set), len(sym_set)))
+    print('Copying %i files' % len(files_set))    
+    for path in sorted(files_set):   
+        copy_file(path, root, scr)
+
+    print('Copying %i symlinks' % len(sym_set))
+    for path in sorted(sym_set):   
+        copy_file(path, root, scr)
+    
     print('==================================')
-    print('cd %s && docker build .' % args.build)
+    if scr is None:
+        print('cd %s && docker build .' % args.build)
+    else:
+        print('cd %s && ./%s && docker build .' % (args.build, args.script))    
+        scr.close()
+    
 if __name__ == "__main__":
 	main()    
